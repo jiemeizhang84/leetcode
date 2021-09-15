21921c4c6d77ae45ea831933159cb69a625d0e8e
Jiemei <jiemei.zhang@icloud.com>
2021-09-14 21:24:22 -0700

    110

---
+++ Tree/110.py
@@ -0,0 +1,21 @@
+class Solution:
+    # Return whether or not the tree at root is balanced while also returning
+    # the tree's height
+    def isBalanced(self, root: TreeNode) -> bool:
+        return self.isBalancedHelper(root)[0]
+    def isBalancedHelper(self, root: TreeNode) -> (bool, int):
+        # An empty tree is balanced and has height -1
+        if not root:
+            return True, -1
+        
+        # Check subtrees to see if they are balanced. 
+        leftIsBalanced, leftHeight = self.isBalancedHelper(root.left)
+        if not leftIsBalanced:
+            return False, 0
+        rightIsBalanced, rightHeight = self.isBalancedHelper(root.right)
+        if not rightIsBalanced:
+            return False, 0
+        
+        # If the subtrees are balanced, check if the current tree is balanced
+        # using their height
+        return (abs(leftHeight - rightHeight) < 2), 1 + max(leftHeight, rightHeight)
