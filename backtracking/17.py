c88e79fb15ee86cb13620a6c64190884a7450258
Jiemei <jiemei.zhang@icloud.com>
2021-10-03 22:12:21 -0700

    17

---
+++ backtracking/17.py
@@ -0,0 +1,18 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) == 0:
+            return []
+        hashmap = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", 
+                   "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
+        def backtrack(idx, combo):
+            if len(digits) == len(combo):
+                result.append("".join(combo))
+                return
+            for letter in hashmap[digits[idx]]:
+                combo.append(letter)
+                backtrack(idx + 1, combo)
+                combo.pop()
+                
+        result = []
+        backtrack(0, [])
+        return result
