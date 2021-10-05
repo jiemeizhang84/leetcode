39e9f3e3cd808f2ce2009b2d4489132bc18a313c
Jiemei <jiemei.zhang@icloud.com>
2021-10-04 22:39:45 -0700

    178

---
+++ sql/178.sql
@@ -0,0 +1,8 @@
+SELECT
+  Score,
+  (SELECT count(*) FROM (SELECT distinct Score s FROM Scores) tmp WHERE s >= Score) `Rank`
+FROM Scores
+ORDER BY Score desc
+
+select Score, dense_rank() over (order by Score desc) as `Rank`
+from scores
