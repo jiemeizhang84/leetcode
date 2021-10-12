0781ebef916f02720a81743288d16bc29e9b8903
Jiemei <jiemei.zhang@icloud.com>
2021-10-11 23:48:00 -0700

    973

---
+++ heap:priority queue/973.py
@@ -0,0 +1,10 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        pq = []
+        for (x, y) in points:
+            dist = -(x*x + y*y)
+            if len(pq) == k:
+                heapq.heappushpop(pq, (dist, x, y))
+            else:
+                heapq.heappush(pq, (dist, x, y))
+        return [(x, y) for (dist, x, y) in pq]
