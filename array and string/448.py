a16b9f901f1a8a6421da790e89b61bc884b3f4b2
Jiemei <jiemei.zhang@icloud.com>
2021-10-22 23:36:17 -0700

    448

---
+++ array and string/448.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        res = []
+        for num in nums:
+            if nums[abs(num) - 1] > 0:
+                nums[abs(num) - 1] *= -1
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                res.append(i + 1)
+        return res   
