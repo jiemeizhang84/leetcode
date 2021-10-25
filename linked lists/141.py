93d137bc1b261f7a8598b1ff500b4f2a59b7e821
Jiemei <jiemei.zhang@icloud.com>
2021-10-25 00:09:41 -0700

    141

--- linked lists/141.py
+++ linked lists/141.py
@@ -1,12 +1,9 @@
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
-        if head is None:
-            return False
-        slow = head
-        fast = head.next
-        while slow != fast:
-            if fast is None or fast.next is None:
-                return False
+        slow, fast = head, head
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        return True
+            if slow == fast:
+                return True
+        return False
