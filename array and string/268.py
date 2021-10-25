fa6877a35da3f0aa137d5b2c105480dd4a41c895
Jiemei <jiemei.zhang@icloud.com>
2021-10-24 21:12:45 -0700

    53

--- array and string/268.py
+++ array and string/268.py
@@ -6,3 +6,13 @@ class Solution:
             res += (i - nums[i])
             
         return res
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        res = len(nums)
+        
+        for i in range(len(nums)):
+            res ^= i ^ nums[i]
+            
+        return res    
--- linked lists/206.py
+++ linked lists/206.py
@@ -7,3 +7,16 @@ class Solution:
             prev = head
             head = tmp
         return prev
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        if not head:
+            return None
+        new_head = head
+        if head.next:
+            new_head = self.reverseList(head.next)
+            head.next.next = head
+            head.next = None
+        return new_head
