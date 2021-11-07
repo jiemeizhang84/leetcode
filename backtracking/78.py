30191bfbeb00db027d70639d5e8adad28a2e3959
Jiemei <jiemei.zhang@icloud.com>
2021-11-07 13:41:02 -0800

    78

--- backtracking/78.py
+++ backtracking/78.py
@@ -1,6 +1,21 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
+        
+        def dfs(path, start):
+            res.append(path.copy())
+            for i in range(start, len(nums)):
+                path.append(nums[i])
+                dfs(path, i + 1)
+                path.pop()
+        
+        dfs([], 0)
+        return res
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
         subset = []
         self.dfs(nums, subset, res, 0)
         return res        
