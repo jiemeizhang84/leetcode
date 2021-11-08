3f3ada037d6428296b6222cdd7c3fd676fd3143c
Jiemei <jiemei.zhang@icloud.com>
2021-11-07 21:09:19 -0800

    77

---
+++ backtracking/77.py
@@ -0,0 +1,15 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        
+        def dfs(path, start):
+            if len(path) == k:
+                res.append(path.copy())
+                return
+            for i in range(start, n + 1):
+                path.append(i)
+                dfs(path, i + 1)
+                path.pop()
+        
+        dfs([], 1)
+        return res
