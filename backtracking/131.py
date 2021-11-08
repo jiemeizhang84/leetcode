892230452ca6fabca8a2134bb8c911f3ff53a9e6
Jiemei <jiemei.zhang@icloud.com>
2021-11-07 23:00:00 -0800

    131

--- backtracking/131.py
+++ backtracking/131.py
@@ -1,19 +1,19 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res = []
-        self.dfs(s, [], res, 0)
-        return res
         
-    def dfs(self, s, part, res, i):
-        if i >= len(s):
-            res.append(part.copy())
-            return
-        for j in range(i, len(s)):
-            if self.is_palindrome(s, i, j):
-                part.append(s[i:j+1])
-                self.dfs(s, part, res, j+1)
-                part.pop()
-        
+        def dfs(path, start):
+            if start == len(s):
+                res.append(path.copy())
+                return
+            for i in range(start, len(s)):
+                if self.is_palindrome(s, start, i):
+                    path.append(s[start: i + 1])
+                    dfs(path, i + 1)
+                    path.pop()
+       
+        dfs([], 0)
+        return res        
         
     def is_palindrome(self, s, l, r):
         while l < r:
