6c59f805e87a2b3922ab5df8d6c9223cefddc40c
Jiemei <jiemei.zhang@icloud.com>
2021-11-15 22:48:43 -0800

    647

--- dynamic programming/647.py
+++ dynamic programming/647.py
@@ -13,3 +13,25 @@ class Solution:
                 l -= 1
                 r += 1
         return res
+
+#DP
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        N = len(s)
+        res = 0
+        if N == 0: return res
+        dp = [[False] * N for _ in range(N)]
+        print(dp)
+        for i in range(N):
+            dp[i][i] = True
+            res += 1
+        for i in range(N - 1):
+            dp[i][i + 1] = s[i] == s[i + 1]
+            res += 1 if dp[i][i + 1] else 0
+            
+        for length in range(2, N):
+            for i in range(N - length):
+                dp[i][i + length] = dp[i + 1][i + length - 1] and s[i] == s[i + length]
+                res += 1 if dp[i][i + length] else 0
+                
+        return res
