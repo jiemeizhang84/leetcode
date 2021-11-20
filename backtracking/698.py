4468449fb5f23e6d37842daa1200371f3594584a
Jiemei <jiemei.zhang@icloud.com>
2021-11-19 21:49:34 -0800

    698

---
+++ backtracking/698.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        if sum(nums) % k:
+            return False
+        nums.sort(reverse=True)
+        target = sum(nums) / k
+        used = [False] * len(nums)
+        
+        def backtrack(i, k, subsum):
+            if k == 0:
+                return True
+            if subsum == target:
+                return backtrack(0, k - 1, 0)
+            for j in range(i, len(nums)):
+                if used[j] or subsum + nums[j] > target:
+                    continue
+                used[j] = True
+                if backtrack(j + 1, k, subsum + nums[j]):
+                    return True
+                used[j] = False
+            return False
+        
+        return backtrack(0, k, 0)
